a610d307d8c0f3d286cb8cae51cfddf76a7c7a49
Aurélio Moreira <aureliomoreirared@gmail.com>
2023-07-17 12:51:03 -0300

    deleting unnecessary files

--- src/infrastructure/utils/validators/dateValidator.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-restricted-globals */
-export abstract class DateValidator {
-  static isValidDate(value: any): value is Date {
-    return value instanceof Date && !isNaN(value.getTime());
-  }
-}
--- src/infrastructure/utils/validators/enumValidator.ts
+++
@@ -1,8 +0,0 @@
-interface IValidatableEnum<T> {
-  [key: string]: T;
-}
-export abstract class EnumValidator {
-  static isEnumValue<T>(value: any, enumObj: IValidatableEnum<T>): value is T {
-    return Object.values(enumObj).includes(value);
-  }
-}
